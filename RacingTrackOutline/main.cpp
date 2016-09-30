2d02f11fb39452a53503477e08495b7df7f13dd6
Marvin Aquino <marvingonaq@marvins-macbook-air.local>
2016-09-29 21:50:29 -0300

    First commit, working getting video stream

--- RacingTrackOutline/main.cpp
+++ RacingTrackOutline/main.cpp
@@ -6,10 +6,45 @@
 //  Copyright © 2016 Marvin Aquino. All rights reserved.
 //
 
+#ifdef __WIN32
+    #include <Windows.h>
+#elif __APPLE__
+    #include <opencv.hpp>
+    #include <highgui.hpp>
+#endif
 #include <iostream>
+#include <opencv.hpp>
+#include <highgui.hpp>
+
+cv::Mat Video;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cv::VideoCapture cap("/Users/marvingonaq/Documents/RacingTrackOutline/RacingTrackOutline/Full\ Lap\ Nurburgring\ Nissan\ -\ GTR.mp4");
+    if(!cap.isOpened()){
+        std::cout << "Error while opening file" << std::endl;
+        return -1;
+    }
+    cap.set(CV_CAP_PROP_POS_MSEC, 300);
+    double fps = cap.get(CV_CAP_PROP_FPS);
+    std::cout << "Frames per Second: " << fps << std::endl;
+    cv::namedWindow("Video",CV_WINDOW_AUTOSIZE);
+    while(1){
+        cv::Mat frame;
+        bool bSuccess = cap.read(frame);
+        
+        if(!bSuccess){
+            std::cout << "Cannot get frame from Video" << std::endl;
+            break;
+        }
+        imshow("Video",frame);//Show current frame in "Video" window
+        if(cv::waitKey(30)==27){
+            std::cout << "'ESC' key has been pressed, closing video." << std::endl;
+            cv:cvDestroyWindow("Video");
+            break;
+        }
+    }
+    //std::cout << "Using OpenCV" << CV_VERSION << "/n" <<std::endl;
+    //std::cout << cv::getBuildInformation();
     return 0;
 }
